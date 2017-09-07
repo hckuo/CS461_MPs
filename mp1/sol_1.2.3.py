659a53e4b3dc6ed04361e06a5be6eb2968dda738
mhasan11 <mhasan11@illinois.edu>
2017-09-07 02:30:38 -0400

    minor

--- mp1/sol_1.2.3.py
+++ mp1/sol_1.2.3.py
@@ -1,4 +1,7 @@
 import urllib2
+import binascii as ba
+
+
 def get_status(u):
     req = urllib2.Request(u)
     try:
@@ -18,9 +21,27 @@ def read_from_file(filename):
 
 if __name__=='__main__':
 
-    hexdata = read_from_file("1.2.3_ciphertext.hex")
-    hexdata = hex(hexdata)
-    #print(hexdata)
+    netid = "mhasan11"
 
-    blocksize = 16
-    print("number of blocks", hexdata/blocksize)
+    hexdata = read_from_file("1.2.3_ciphertext.hex")
+    # hexdata = ba.unhexlify(hexdata)
+    print(hexdata)
+
+    blocksize = 16  # we have 16 bytes block
+    n_blocks = len(hexdata)/blocksize # total number of blocks
+
+    # this is the list of all blocks
+    block_list = list(hexdata[0+i:blocksize+i] for i in range(0, len(hexdata), blocksize))
+
+    # print(n_blocks)
+    # print(block_list)
+
+    iv = "".zfill(blocksize)  # the initialization vector, set to zero
+
+    # print(iv+block_list[0])
+    msg = iv+block_list[0]  # TODO we need to update this iteratively
+    # msg_url = "http://192.17.90.133:9999/mp1/${"+netid+"}/?"+msg
+    msg_url = "http://192.17.90.133:9999/mp1/"+netid+"/?"+hexdata
+    print(msg_url)
+    # get_status(msg_url)
+    get_status(msg_url)
