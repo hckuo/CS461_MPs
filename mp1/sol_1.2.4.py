2e8fcc391fbedcaaf0c391fe44711b986fe8276f
Hsuan-Chi Kuo <auszon3@gmail.com>
2017-09-09 23:52:53 -0500

    Done 1.2.4
    It takes roughly 2 minutes to find the answer on my laptop.

--- mp1/sol_1.2.4.py
+++ mp1/sol_1.2.4.py
@@ -1,70 +1,66 @@
-from __future__ import division
-from operator import mul
 import numpy as np
 from fractions import gcd
-
+from Crypto.PublicKey import RSA
+from sets import Set
+from pbp import decrypt
+import sys
 
 def read_from_file(filename):
     with open(filename, 'r') as f:
-        content = f.read().strip()
+        content = f.read()
         f.close()
     return content
 
+def extended_gcd(aa, bb):
+    lastremainder, remainder = abs(aa), abs(bb)
+    x, lastx, y, lasty = 0, 1, 1, 0
+    while remainder:
+        lastremainder, (quotient, remainder) = remainder, divmod(lastremainder, remainder)
+        x, lastx = lastx - quotient*x, x
+        y, lasty = lasty - quotient*y, y
+    return lastremainder, lastx * (-1 if aa < 0 else 1), lasty * (-1 if bb < 0 else 1)
 
-def get_private_key(e, p, q):
-    # follow the equation in Section 2.1 of the paper
-    
-    d = (1/e) % ( (p-1) * (q-1) )
-    return d
-
+def modinv(a, m):
+    g, x, y = extended_gcd(a, m)
+    if g != 1:
+	    raise ValueError
+    return x % m
 
 if __name__=='__main__':
-    
+
     mod_filename = 'moduli.hex'
-    
-    public_e = 65537
-    
+    ciphertext_file = sys.argv[1]
+    output_file = sys.argv[2]
+    ciphertext = read_from_file(ciphertext_file)
+    public_e = 65537L
+
     moduli = read_from_file(mod_filename)
     moduli = moduli.splitlines()
-    
-    ########################################################
-    # NOTE: Commnet this next line for Full list of 10100 moduli
-    ########################################################
-    moduli = moduli[0:100]  # for testing shorten the list
-    
-    # change into int
     moduli = [int(moduli[i], 16) for i in range(0, len(moduli))]
-    
-    #print moduli
-    #print len(moduli)
-    
-    product = reduce(mul, moduli, 1)
-    #print "Product is" , product
-    
+
+    common_factors = Set()
+    working_window = moduli
+    product = np.product(working_window)
     # mining p and q
     # following the Algorithm in Section 3.3
-    pi = []
-    qi = []
-    for i in range(0, len(moduli)):
-        
+    for i in range(0, len(working_window)):
         ni2 = np.power(moduli[i], 2)
         zi  = product % ni2
         p = gcd(moduli[i],zi/moduli[i])
-        pi.append(p)
-        qi.append(moduli[i] / p)
-   
-    
-    print pi
-    #print qi
-    
-    
-    private_key = []
-    for i in range(0, len(moduli)):
-        d = get_private_key(public_e, pi[i], qi[i])
-        print d
-        private_key.append(d)
-    
-    print private_key
-    # TODO: need to depcrypt the file using public key
-    
-    print "end"
+	if p == 1:
+	    continue
+        q = moduli[i] / p
+	try:
+	    private_d = modinv(public_e, (p-1)*(q-1))
+	except ValueError:
+	    continue
+	tup = (moduli[i], public_e, private_d, p, q)
+        rsakey = RSA.construct(tup)
+        try:
+            plaintext = decrypt(rsakey, ciphertext)
+            print i, 'Correct RSA Key!'
+            with open(output_file, 'w') as f:
+                f.write(plaintext)
+            break
+        except ValueError:
+            print i, 'Wrong RSA Key'
