c18363aadd6117b48e24cd8f83a7316177aab567
Hsuan-Chi Kuo <auszon3@gmail.com>
2017-09-13 13:02:58 -0500

    add comments in 1.2.5.sh

--- mp1/1.2.5.sh
+++ mp1/1.2.5.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 netid=hckuo2
-python tweakedCertbuilder.py $netid template.cer
-dd bs=1 count=192 skip=4 if=template.cer of=prefix.cer
-IV=$(openssl dgst -md5 prefix.cer | awk '{print $2}')
+python tweakedCertbuilder.py $netid template.cer # generate tempate certificate
+dd bs=1 count=192 skip=4 if=template.cer of=prefix.cer # truncate "the to-be-signed" part where the modulus bytes start as prefix
+IV=$(openssl dgst -md5 prefix.cer | awk '{print $2}') # compute md5(prefix) as inital vector to produce collisions with fastcoll
 echo $IV
 ./fastcoll -i $IV -o b1file b2file
-python 1.2.5.py b1file b2file
+python 1.2.5.py b1file b2file # use these two collisions files as inputs as b1, b2 described in the paper
 
