1e183b9f7da3a6f52fe645c8c1433ff428203071
Hsuan-Chi Kuo <auszon3@gmail.com>
2017-09-13 10:13:40 -0500

    fix typo in 1.2.5.sh

--- mp1/1.2.5.sh
+++ mp1/1.2.5.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 netid=hckuo2
-python tweakCertbuilder.py $netid template.cer
+python tweakedCertbuilder.py $netid template.cer
 dd bs=1 count=256 skip=4 if=template.cer of=prefix.cer
 IV=$(openssl dgst -md5 prefix.cer | awk '{print $2}')
 echo $IV
