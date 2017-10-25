75d2d25adf285b99b2891d840582d0bde29057a2
Monowar <mhasan11@illinois.edu>
2017-10-25 18:00:50 -0500

    fix 3.2.2

--- mp3/3.2.2.py
+++ mp3/3.2.2.py
@@ -7,15 +7,13 @@ import socket
 def main():
     if (len(sys.argv) < 2):
         print "error: need argument"
-        # sys.exit(1)
+        sys.exit(1)
 
-    #filename = sys.argv[1]
-    filename = "lbl-internal.20041004-1305.port002.dump.anon.pcap"
-    #filename = "lbl-internal.20041004-1305.port002.dump.anon"
-    #filename = '3.1.1.pcap'
+    filename = sys.argv[1]
+    #filename = "lbl-internal.20041004-1305.port002.dump.anon.pcap"
     print "input filename: " + filename
     
-    f = open(filename, 'rb')
+    f = open(filename)
     pcap = dpkt.pcap.Reader(f)
     
     #count = 0
@@ -23,51 +21,53 @@ def main():
     save_syn = {}
     save_syn_ack ={}
 
+   
     
     for ts, buf in pcap:
-        #print ts, len(buf)
         
-#        print count
-#        count += 1
         try: 
             eth = dpkt.ethernet.Ethernet(buf)
+        
         except Exception:    
-            #print "wrong parse!"
             continue
+    
         
         if eth.type == dpkt.ethernet.ETH_TYPE_IP:
-           
             ip = eth.data
-        
-            # if it's tcp packet
+            
             if ip.p == dpkt.ip.IP_PROTO_TCP:
-                #print "in if"
                 tcp = ip.data
                 
                 src = socket.inet_ntoa(ip.src)
                 dst = socket.inet_ntoa(ip.dst)
                 
-                #print "src: " + src + "dst: " + dst
+                # print "src: " + src + "dst: " + dst
                 
                 if tcp.flags & dpkt.tcp.TH_SYN != 0:
+                    
                     if tcp.flags & dpkt.tcp.TH_ACK != 0:
-                        if ip not in save_syn_ack :
+                        
+                        # print "in if!"
+                        
+                        if dst not in save_syn_ack :
                             save_syn_ack[dst] = 0
                         save_syn_ack[dst] += 1
+                   
                     else:
                         if src not in save_syn :
                             save_syn[src] = 0
                         save_syn[src] += 1
-                
-                
-            
-    for adrs in save_syn:
-        if adrs not in save_syn_ack:
-            save_syn_ack[adrs] = 0
-            if save_syn[adrs] > save_syn_ack[adrs] * 3:
-                print adrs
     
-    print "script finished!"
+    for address in save_syn:
+        
+        if address not in save_syn_ack :
+            # print "not here"
+            save_syn_ack[address] = 0
+        
+        if save_syn[address] > save_syn_ack[address] * 3:
+            print address
+    
+    # print "script finished!"
     #sys.exit(0)
 
 
