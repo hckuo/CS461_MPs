21c9ca5e7b48cade656b8f5d6d5b75b38423889d
Monowar <mhasan11@illinois.edu>
2017-10-25 18:03:43 -0500

    minor

--- mp3/3.2.2.py
+++ mp3/3.2.2.py
@@ -64,7 +64,7 @@ def main():
             # print "not here"
             save_syn_ack[address] = 0
         
-        if save_syn[address] > save_syn_ack[address]*3:
+        if save_syn[address] > 3*save_syn_ack[address]:
             print address
     
     # print "script finished!"
