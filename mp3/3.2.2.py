be9bca6dae2a3e9b7500bcf4893db16789b7dac6
Hsuan-Chi Kuo <auszon3@gmail.com>
2017-10-30 11:03:45 -0500

    done 3.2.1.[34].txt

--- mp3/3.2.2.py
+++ mp3/3.2.2.py
@@ -12,63 +12,63 @@ def main():
     filename = sys.argv[1]
     #filename = "lbl-internal.20041004-1305.port002.dump.anon.pcap"
     print "input filename: " + filename
-    
+
     f = open(filename)
     pcap = dpkt.pcap.Reader(f)
-    
+
     #count = 0
-    
+
     save_syn = {}
     save_syn_ack ={}
 
-   
-    
+
+
     for ts, buf in pcap:
-        
-        try: 
+
+        try:
             eth = dpkt.ethernet.Ethernet(buf)
-        
-        except Exception:    
+
+        except Exception:
             continue
-    
-        
+
+
         if eth.type == dpkt.ethernet.ETH_TYPE_IP:
             ip = eth.data
-            
+
             if ip.p == dpkt.ip.IP_PROTO_TCP:
                 tcp = ip.data
-                
+
                 src = socket.inet_ntoa(ip.src)
                 dst = socket.inet_ntoa(ip.dst)
-                
+
                 # print "src: " + src + "dst: " + dst
-                
+
                 if tcp.flags & dpkt.tcp.TH_SYN != 0:
-                    
+
                     if tcp.flags & dpkt.tcp.TH_ACK != 0:
-                        
+
                         # print "in if!"
-                        
+
                         if dst not in save_syn_ack:
                             save_syn_ack[dst] = 0
                         save_syn_ack[dst] += 1
-                   
+
                     else:
                         if src not in save_syn:
                             save_syn[src] = 0
                         save_syn[src] += 1
-    
+
     for address in save_syn:
-        
+
         if address not in save_syn_ack:
             # print "not here"
             save_syn_ack[address] = 0
-        
+
         if save_syn[address] > 3*save_syn_ack[address]:
             print address
-    
+
     # print "script finished!"
-    #sys.exit(0)
+    sys.exit(0)
 
 
 if __name__ == '__main__':
